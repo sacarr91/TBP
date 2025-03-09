7c8584c88d78d30fa5b15ad66bb059d04b1dc316
Stephanie Carr <sacarr91@gmail.com>
2025-03-09 14:37:59 -0400

    updated pathing on all sources

--- NTSGgithub/js/nav.js
+++ NTSGgithub/js/nav.js
@@ -3,9 +3,10 @@ const theNavbar = document.getElementById(`header`)
 const injectNav = () => {
     const nav = `<nav class="navbar navbar-expand-lg bg-body-tertiary mb-3 p-3">
             <div class="container-fluid">
-                <img src="../assets/images/CSEE (2).png" alt="Logo" href="#" height="60px"
+                <img src="/NTSGgithub/assets/images/NTSGlogo.png" alt="Logo" href="#" height="60px"
                     class="d-inline-block align-text-center">
-                <a class="navbar-brand px-3" href="../../index.html" style="font-size: xx-large;">New to Strings Guide</a>
+                <a class="navbar-brand px-3" href="/NTSGgithub/index.html" style="font-size: xx-large;">New to Strings
+                    Guide</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                     data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false"
                     aria-label="Toggle navigation">
@@ -15,14 +16,17 @@ const injectNav = () => {
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                         <ul class="nav nav-pills">
                             <li class="nav-item">
-                                <a class="nav-link" style="text-align: center;" href="../pages/starting.html">Starting an Orchestra</a>
+                                <a class="nav-link" style="text-align: center;" href="/NTSGgithub/pages/starting.html">Starting an
+                                    Orchestra</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" style="text-align: center;" href="../pages/maintaining.html">Maintaining an
+                                <a class="nav-link" style="text-align: center;"
+                                    href="/NTSGgithub/pages/maintaining.html">Maintaining an
                                     Orchestra</a>
                             </li>
                             <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="pages/repertoire/repertoire.html" role="button"
+                                <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown"
+                                    href="/NTSGgithub/pages/repertoire/repertoire.html" role="button"
                                     aria-expanded="false">Repertoire</a>
                                 <ul class="dropdown-menu">
                                     <li><a class="dropdown-item" href="#">ES</a></li>
@@ -33,14 +37,19 @@ const injectNav = () => {
                                 </ul>
                             </li>
                             <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="pages/pedagogy/pedagogy.html" role="button"
+                                <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown"
+                                    href="/NTSGgithub/pages/pedagogy/pedagogy.html" role="button"
                                     aria-expanded="false">Pedagogy</a>
                                 <ul class="dropdown-menu">
-                                    <li><a class="dropdown-item" href="../pages/pedagogy/pedagogy.html">Overview</a></li>
-                                    <li><a class="dropdown-item" href="../pages/pedagogy/violin-pedagogy.html">Violin</a></li>
-                                    <li><a class="dropdown-item" href="../pages/pedagogy/viola-pedagogy.html">Viola</a></li>
-                                    <li><a class="dropdown-item" href="../pages/pedagogy/cello-pedagogy.html">Cello</a></li>
-                                    <li><a class="dropdown-item" href="../pages/pedagogy/bass-pedagogy.html">Bass</a></li>
+                                    <li><a class="dropdown-item" href="/NTSGgithub/pages/pedagogy/pedagogy.html">Overview</a></li>
+                                    <li><a class="dropdown-item" href="/NTSGgithub/pages/pedagogy/violin-pedagogy.html">Violin</a>
+                                    </li>
+                                    <li><a class="dropdown-item" href="/NTSGgithub/pages/pedagogy/viola-pedagogy.html">Viola</a>
+                                    </li>
+                                    <li><a class="dropdown-item" href="/NTSGgithub/pages/pedagogy/cello-pedagogy.html">Cello</a>
+                                    </li>
+                                    <li><a class="dropdown-item" href="/NTSGgithub/pages/pedagogy/bass-pedagogy.html">Bass</a>
+                                    </li>
                                     <li>
                                         <hr class="dropdown-divider">
                                     </li>
@@ -48,8 +57,8 @@ const injectNav = () => {
                                 </ul>
                             </li>
                             <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="pages/FOA/FOA.html" role="button"
-                                    aria-expanded="false">Guide to FOA</a>
+                                <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="/NTSGgithub/pages/FOA/FOA.html"
+                                    role="button" aria-expanded="false">Guide to FOA</a>
                                 <ul class="dropdown-menu">
                                     <li><a class="dropdown-item" href="#">FOA 1</a></li>
                                     <li><a class="dropdown-item" href="#">FOA 2</a></li>
@@ -62,11 +71,10 @@ const injectNav = () => {
                             </li>
 
                             <li class="nav-item">
-                                <a class="nav-link" 
-                                href="../pages/tools.html">Useful Tools</a>
+                                <a class="nav-link" href="/NTSGgithub/pages/tools.html">Useful Tools</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="../pages/resources.html">Resources</a>
+                                <a class="nav-link" href="/NTSGgithub/pages/resources.html">Resources</a>
                             </li>
 
                         </ul>
@@ -75,4 +83,4 @@ const injectNav = () => {
     theNavbar.innerHTML += nav;
 };
 
-// injectNav();
+injectNav();
