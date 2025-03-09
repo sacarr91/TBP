ed528820f3d605632fed6762f6c4adddeeef5656
Stephanie Carr <sacarr91@gmail.com>
2025-03-09 18:41:17 -0400

    updated homepage hero img & removed modal js

--- NTSGgithub/js/home.js
+++ NTSGgithub/js/home.js
@@ -3,7 +3,7 @@ const heroDiv = document.querySelector("#heroImg");
 const credsDiv = document.querySelector("#credentials");
 
 const injectHero = () => {
-    const heroImg = `<img src="./assets/images/Compiled by Stephanie Carr, Florida State University MME.png" class="img-fluid" alt="New-To-Strings Guide: A compilation of expert teaching, useful resources, reproducible materials,
+    const heroImg = `<img src="/NTSGgithub/assets/images/homepageHero.png" class="img-fluid" alt="New-To-Strings Guide: A compilation of expert teaching, useful resources, reproducible materials,
         advice from native string players, and other burden-easing tidbits for the new-
         to-strings teacher. Compiled by Stephanie Carr, MME under the advisement of Dr. Kasia Bugaj, FSU">`;
     heroDiv.innerHTML += heroImg;
