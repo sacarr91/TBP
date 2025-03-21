05aec09d603e13b5bedd1a9a8a1ddc346872137c
Stephanie Carr <sacarr91@gmail.com>
2025-03-21 10:55:59 -0400

    updated NTSG path in js

--- pages/NTSG/js/NTSGhome.js
+++ pages/NTSG/js/NTSGhome.js
@@ -3,7 +3,7 @@ const heroDiv = document.querySelector("#heroImg");
 const credsDiv = document.querySelector("#credentials");
 
 const injectHero = () => {
-    const heroImg = `<img src="/NTSGgithub/assets/images/homepageHero.png" class="img-fluid" alt="New-To-Strings Guide: A compilation of expert teaching, useful resources, reproducible materials,
+    const heroImg = `<img src="/pages/NTSG/assets/images/homepageHero.png" class="img-fluid" alt="New-To-Strings Guide: A compilation of expert teaching, useful resources, reproducible materials,
         advice from native string players, and other burden-easing tidbits for the new-
         to-strings teacher. Compiled by Stephanie Carr, MME under the advisement of Dr. Kasia Bugaj, FSU">`;
     heroDiv.innerHTML += heroImg;
