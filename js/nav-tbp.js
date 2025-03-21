902ffc1ba7bc326704634d89741a2be9a236a0b3
Stephanie Carr <sacarr91@gmail.com>
2025-03-21 11:18:29 -0400

    added html to new tbp pages

--- js/nav-tbp.js
+++ js/nav-tbp.js
@@ -1,88 +1,42 @@
 const TbpNavbar = document.getElementById(`header`)
 
-const injectNav = () => {
+const injectTbpNav = () => {
     const nav = `<nav class="navbar navbar-expand-lg bg-body-tertiary mb-3 p-3">
-            <div class="container-fluid">
-                <img src="/NTSGgithub/assets/images/NTSGlogo.png" alt="Logo" href="#" height="60px"
-                    class="d-inline-block align-text-center">
-                <a class="navbar-brand px-3" href="/NTSGgithub/index.html" style="font-size: xx-large;">New to Strings
-                    Guide</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false"
-                    aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <ul class="nav nav-pills">
+        <div class="container-fluid">
+            <img src="/assets/images/TBPlogo.png" alt="Logo" href="/index-2.html" height="60px"
+                class="d-inline-block align-text-center">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02"
+                aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <ul class="nav nav-pills">
                         <li class="nav-item">
-                                <a class="nav-link" style="text-align: center;" href="/NTSGgithub/index.html">Home</a>
-                            <li class="nav-item">
-                                <a class="nav-link" style="text-align: center;" href="/NTSGgithub/pages/starting.html">Starting an
-                                    Orchestra</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" style="text-align: center;"
-                                    href="/NTSGgithub/pages/maintaining.html">Maintaining an
-                                    Orchestra</a>
-                            </li>
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown"
-                                    href="/NTSGgithub/pages/repertoire/repertoire.html" role="button"
-                                    aria-expanded="false">Repertoire</a>
-                                <ul class="dropdown-menu">
-                                    <li><a class="dropdown-item" href="/NTSGgithub/pages/repertoire/repertoire.html">Overview</a></li>
-                                    <li><a class="dropdown-item" href="#">ES</a></li>
-                                    <li><a class="dropdown-item" href="#">DS</a></li>
-                                    <li><a class="dropdown-item" href="#">CS</a></li>
-                                    <li><a class="dropdown-item" href="#">BS</a></li>
-                                    <li><a class="dropdown-item" href="#">AS</a></li>
-                                </ul>
-                            </li>
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown"
-                                    href="/NTSGgithub/pages/pedagogy/pedagogy.html" role="button"
-                                    aria-expanded="false">Pedagogy</a>
-                                <ul class="dropdown-menu">
-                                    <li><a class="dropdown-item" href="/NTSGgithub/pages/pedagogy/pedagogy.html">Overview</a></li>
-                                    <li><a class="dropdown-item" href="/NTSGgithub/pages/pedagogy/violin-pedagogy.html">Violin</a>
-                                    </li>
-                                    <li><a class="dropdown-item" href="/NTSGgithub/pages/pedagogy/viola-pedagogy.html">Viola</a>
-                                    </li>
-                                    <li><a class="dropdown-item" href="/NTSGgithub/pages/pedagogy/cello-pedagogy.html">Cello</a>
-                                    </li>
-                                    <li><a class="dropdown-item" href="/NTSGgithub/pages/pedagogy/bass-pedagogy.html">Bass</a>
-                                    </li>
-                                    <li>
-                                        <hr class="dropdown-divider">
-                                    </li>
-                                    <li><a class="dropdown-item" href="#">Guitar</a></li>
-                                </ul>
-                            </li>
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="/NTSGgithub/pages/FOA/FOA.html"
-                                    role="button" aria-expanded="false">Guide to FOA</a>
-                                <ul class="dropdown-menu">
-                                    <li><a class="dropdown-item" href="/NTSGgithub/pages/FOA/FOA.html">What is FOA?</a></li>
-                                    <li>
-                                        <hr class="dropdown-divider">
-                                    </li>
-                                    <li><a class="dropdown-item" href="#">All-State</a></li>
-                                    <li><a class="dropdown-item" href="#">Solo & Ensemble</a></li>
-                                    <li><a class="dropdown-item" href="#">Concert MPA</a></li>
-                                </ul>
-                            </li>
-
-                            <li class="nav-item">
-                                <a class="nav-link" href="/NTSGgithub/pages/tools.html">Useful Tools</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="/NTSGgithub/pages/resources.html">Resources</a>
-                            </li>
-
-                        </ul>
-                </div>
-        </nav>`;
+                            <a class="nav-link" style="text-align: center;" href="/index-2.html">Home</a>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown"
+                                href="/pages/Toolbox/toolbox.html" role="button"
+                                aria-expanded="false">Toolbox</a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="/pages/Toolbox/activities.html">Activities</a></li>
+                                <li><a class="dropdown-item" href="/pages/Toolbox/resources.html">Resources</a></li>
+                                <li><a class="dropdown-item" href="/pages/Toolbox/tips.html">Tips</a></li>
+                                <li><a class="dropdown-item" href="/pages/Toolbox/DCs-hosts.html">District Chairs & Hosts</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/pages/deepdive.html">Deep Dive</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/pages/contact.html">Contact</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/pages/NTSG/index.html">New to Strings Guide</a>
+                        </li>
+                    </ul>
+            </div>
+    </nav>`;
     TbpNavbar.innerHTML += nav;
 };
 
