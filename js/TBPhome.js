cdf83b491a1b1dab1f5a4acd0fd99f54fa5def58
Stephanie Carr <sacarr91@gmail.com>
2025-03-23 18:04:42 -0400

    completed TBP hero image

--- js/TBPhome.js
+++ js/TBPhome.js
@@ -1,7 +1,7 @@
 const heroDiv = document.querySelector("#heroImg");
 
 const injectHero = () => {
-    const heroImg = `<img src="/assets/images/TBPheroImg.png" class="img-fluid" alt="The Balance Point: Resources and ideas for the 21st-century string music educator">`;
+    const heroImg = `<img src="/assets/images/TBPheroImg3.png" class="img-fluid" alt="The Balance Point: Resources and ideas for the 21st-century string music educator">`;
     heroDiv.innerHTML += heroImg;
 };
 
