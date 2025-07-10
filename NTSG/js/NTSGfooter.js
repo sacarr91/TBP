673bdba6f327c24432e416a5c13836eb6d3e422f
Stephanie Carr <sacarr91@gmail.com>
2025-07-10 15:17:33 -0400

    changed text in footer. Still silly.

--- NTSG/js/NTSGfooter.js
+++ NTSG/js/NTSGfooter.js
@@ -2,7 +2,7 @@ const theFooter = document.getElementById(`footer`)
 
 const injectFooter = () => {
     const foot = `<div style="text-align: center;">
-            <h1><b>~Hello, World!~</b></h1>
+            <h1><b>~Hello, Footer!~</b></h1>
         </div>`;
     theFooter.innerHTML += foot;
 };
