e9b6c562e789f647b3ef3652617a2d375c7c3ad3
Stephanie Carr <sacarr91@gmail.com>
2025-07-10 17:48:09 -0400

    usable footer

--- NTSG/js/NTSGfooter.js
+++ NTSG/js/NTSGfooter.js
@@ -1,9 +1,13 @@
 const theFooter = document.getElementById(`footer`)
 
 const injectFooter = () => {
-    const foot = `<div style="text-align: center;">
-            <h1><b>~Hello, Footer!~</b></h1>
-        </div>`;
+    const foot = `<nav class="navbar navbar-dark bg-dark">
+            <div class="container-fluid" style="justify-content: end;">
+                <a class="navbar-brand" href="http://balancepointfl.com/">
+                    Ready for more? Check out <img src="/assets/images/TBPlogo.png" width="30"
+                        style="margin-left: 0.5rem;"> The Balance Point</a>
+            </div>
+        </nav>`;
     theFooter.innerHTML += foot;
 };
 
